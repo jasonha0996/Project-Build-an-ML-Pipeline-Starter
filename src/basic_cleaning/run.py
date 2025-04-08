ad6a893ccc2f801fe5722cd53052a0af11a0bd62
Jason Ha <jha10@wgu.edu>
2025-04-08 11:14:15 -0500

    Completed basic cleaning step and logged cleaned data to W&B

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -53,43 +53,43 @@ if __name__ == "__main__":
   
     parser.add_argument(
         "--input_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type =str,
+        help = "Fully-qualified name of the input artifact (e.g., 'sample.csv')",
         required = True
     )
 
     parser.add_argument(
         "--output_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type =str,
+        help = "Name to give to the cleaned dataset artifact (e.g., 'clean_sample.csv')",
         required = True
     )
 
     parser.add_argument(
         "--output_type", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type =str,
+        help = "Type of the artifact being created (e.g., 'cleaned_data')",
         required = True
     )
 
     parser.add_argument(
         "--output_description", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type =str,
+        help = "Short description of the output artifact's content",
         required = True
     )
 
     parser.add_argument(
         "--min_price", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = float,
+        help = "Minimum listing price to include in the cleaned dataset",
         required = True
     )
 
     parser.add_argument(
         "--max_price",
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type =float,
+        help = "Maximum listing price to include in the cleaned dataset",
         required = True
     )
 
